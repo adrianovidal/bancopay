71d31c2db0d488017c59ced368714f66893d619c
avoliveira <adriano.vidal@castgroup.com.br>
2021-02-23 13:22:58 -0300

    CRUD users

--- config/config.exs
+++ config/config.exs
@@ -18,6 +18,10 @@ config :bancopay, BancopayWeb.Endpoint,
   pubsub_server: Bancopay.PubSub,
   live_view: [signing_salt: "nC/aW3d8"]
 
+config :bancopay, Bancopay.Repo,
+  migration_primary_key: [type: :binary_id],
+  migration_foreign_key: [type: :binary_id]
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
